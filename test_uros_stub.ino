57218aa92849e3cd76d7b5142f6c91995e4de655
kaz3w <katsumi@watanabe.email.ne.jp>
2024-10-20 23:05:50 +0900

    UPDATE

--- test_uros_stub.ino
+++ test_uros_stub.ino
@@ -17,7 +17,11 @@
  * CONSTANTS
  **************************************************************************************/
 
+#if defined(ARDUINO_MINIMA)
 static uint32_t const CAN_ID = 0x20;
+#else
+static uint32_t const CAN_ID = 0x21;
+#endif
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -44,7 +48,11 @@ void loop()
   /* Assemble a CAN message with the format of
    * 0xCA 0xFE 0x00 0x00 [4 byte message counter]
    */
+#if defined(ARDUINO_MINIMA)
   uint8_t const msg_data[] = {0xCA,0xFE,0,0,0,0,0,0};
+#else //defined(ARDUINO_UNOWIFIR4)
+  uint8_t const msg_data[] = {0xBE,0xEF,0,0,0,0,0,0};
+#endif
   memcpy((void *)(msg_data + 4), &msg_cnt, sizeof(msg_cnt));
   CanMsg const msg(CanStandardId(CAN_ID), sizeof(msg_data), msg_data);
 
@@ -55,6 +63,7 @@ void loop()
   {
     Serial.print  ("CAN.write(...) failed with error code ");
     Serial.println(rc);
+   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     for (;;) { }
   }
 
@@ -65,17 +74,22 @@ void loop()
 
   /* Only send one message per second. */
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(10);
+  delay(1);
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
 
   if (CAN.available())
   {
+    // for (int ii=0; ii<5; ii++) {
+    //   delay(2);
+    //   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    //   delay(3);
+    //   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    // }
+
     CanMsg const msg = CAN.read();
-    Serial.println(msg);
+    // Serial.println(msg);
   }
 
-  unsigned long elapsed = millis() - cur;
-
-
-  delay(990 - elapsed);                      // wait for a second
+  // unsigned long elapsed = millis() - cur;
+  // delay(250 - elapsed);                      // wait for a second
 }
