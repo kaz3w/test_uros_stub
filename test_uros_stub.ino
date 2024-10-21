9b900f678920aebffedc24ae0eab4435d2db63e3
kaz3w <katsumi@watanabe.email.ne.jp>
2024-10-21 21:30:19 +0900

    UPDATE

--- test_uros_stub.ino
+++ test_uros_stub.ino
@@ -1,9 +1,48 @@
+#include <micro_ros_arduino.h>
+
 
 /**************************************************************************************
  * INCLUDE
  **************************************************************************************/
 
 #include <Arduino_CAN.h>
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+void error_loop(){
+  while(1){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(100);
+  }
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{  
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+  }
+}  
+
+unsigned long prev = millis();
+
 
 /**************************************************************************************
  * CONSTANTS
@@ -21,10 +60,44 @@ static uint32_t const CAN_ID = 0x21;
 
 void setup()
 {
+  set_microros_transports();
+
+
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
   while (!Serial) { }
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_arduino_node_publisher"));
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
+  prev = millis();
+
 
   if (!CAN.begin(CanBitRate::BR_500k))
   {
@@ -84,4 +157,8 @@ void loop()
 
   // unsigned long elapsed = millis() - cur;
   // delay(250 - elapsed);                      // wait for a second
+
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+
+
 }
